7f137398420eb1d97ab8a87d334b7a926de67fa4
MJMrigg <riggmatthew20@outlook.com>
2024-11-24 22:59:47 -0500

    Fixed Encryption with download
    Still fixing Upload.

--- Client.py
+++ Client.py
@@ -4,13 +4,12 @@ import rsa
 from Cryptodome.Cipher import AES
 SIZE = 1024
 FORMAT = "utf-8"
-SERVER_DATA_PATH = "server_data"
 
 # RSA key loading
 keys = [1] # Assume the keys will be sucessfully loaded
 def load_or_generate_keys():
     # Check if keys already exist
-    if os.path.exists("private_key.pem") and os.path.exists("public_key.pem"):
+    if os.path.exists("private_key.pem") and os.path.exists("public_key.pem") and os.path.exists("cipher_key.pem") and os.path.exists("nonce.pem"):
         # Load existing keys
         with open("public_key.pem", "rb") as f:
             public_key = rsa.PublicKey.load_pkcs1(f.read())
@@ -24,12 +23,11 @@ def load_or_generate_keys():
     else:
         print("Error: One or more Encryption Keys have been deleted from your computer")
         keys[0] = 0
-        # Client does not generate their own RSA and AES keys for risk of them being different from the Server's
+        # Client does not generate their own RSA and AES keys because of the risk of them being different from the Server's
 
 
-# Load RSA keys and AES Cipher
+# Load RSA and AES keys
 public_key, private_key, cipher_key, nonce = load_or_generate_keys()
-cipher = AES.new(cipher_key, AES.MODE_EAX, nonce)
 
 # FUNCTION: DOWNLOAD
 def file_download(client_socket, args):
@@ -39,51 +37,76 @@ def file_download(client_socket, args):
     - client_socket: socket object for the client connection
     - args: command arguments containing the filename
     """
-    # Get the file name from the server
-    response = rsa.decrypt(client_socket.recv(SIZE), private_key).decode(FORMAT)
-    if(response == 'File not found'):
-        print("File not found")
-        return
-    if(response == 'Filename required'):
-        print("Filename required")
-        return
-    file_size = int(response)
-    filename = args[0]
 
-    filepath = os.path.join(SERVER_DATA_PATH, filename)
-    number = 0
-    # Check to see if the file already exists. If it does, at a number at the end
-    while(1):
-        if(os.path.exists(filepath)):
-            number += 1
-            filename = filename.split(".")[0]
-            filename = filename.split(" (")
-            filename = filename[len(filename)-2]
-            filename = f"{filename} ({number})"
-            filepath = f"{SERVER_DATA_PATH}\{filename}"
+    # Function to get the Downloads path on the client's machine
+    def get_download_path():
+        import winreg
+        """Returns the default downloads path for linux or windows"""
+        if os.name == 'nt':
+            sub_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
+            downloads_guid = '{374DE290-123F-4565-9164-39C4925E467B}'
+            with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
+                location = winreg.QueryValueEx(key, downloads_guid)[0]
+            return location
         else:
-            break
-        
-    # Prepare to receive the file in chunks
-    with open(filepath, 'wb') as file:
-        received_size = 0
-        received_data = b""
-        while received_size < file_size:
-            # Determine chunk size (use SIZE or remaining bytes if less than SIZE)
-            chunk_size = min(SIZE, file_size - received_size)
-            chunk = client_socket.recv(chunk_size)
-
-            if not chunk:  # End of data
+            return os.path.join(os.path.expanduser('~'), 'downloads')
+    
+    try:
+        # See if the file could be retrieved from the server
+        response = rsa.decrypt(client_socket.recv(SIZE), private_key).decode(FORMAT)
+        if(response == 'File not found'):
+            print("File not found")
+            return
+        if(response == 'Filename required'):
+            print("Filename required")
+            return
+        # If it could, set its path and receive its data
+        file_size = int(response)
+        name = args[0]
+        name = name.split("\\")
+        filename = name[len(name)-1]
+        # Set the path to the client's Downloads folder 
+        downloads = get_download_path()
+        filepath = f"{downloads}\{filename}"
+        number = 0
+        # Check to see if the file already exists in the Downloads folder. If it does, add a number at the end
+        while(1):
+            if(os.path.exists(filepath)):
+                number += 1
+                filetype = filename.split(".")[1]
+                filename = filename.split(".")[0]
+                filename = filename.split(" (")
+                filename = filename[len(filename)-2]
+                filename = f"{filename} ({number}).{filetype}"
+                filepath = f"{downloads}\{filename}"
+            else:
                 break
             
-            received_size += len(chunk) # Update received size
-            received_data = received_data + chunk # Add the data to he received data
-        
-        # Decrypt the data and write it to the file
-        file.write(cipher.decrypt(received_data))
+        # Prepare to receive the file in chunks
+        with open(filepath, 'wb') as file:
+            received_size = 0
+            received_data = b"" # File data
+            while received_size < file_size:
+                # Determine chunk size (use SIZE or remaining bytes if less than SIZE)
+                chunk_size = min(SIZE, file_size - received_size)
+                chunk = client_socket.recv(chunk_size)
 
-    # Confirm download success to client
-    print("File Sucessfully Downloaded")
+                if not chunk:  # End of data
+                    break
+                
+                received_size += len(chunk) # Update received size
+                received_data = received_data + chunk # Add the data to he received data
+            
+            # Decrypt the data using AES and write it to the file
+            decryptor = AES.new(cipher_key, AES.MODE_EAX, nonce)
+            file.write(decryptor.decrypt(received_data))
+            del decryptor
+
+        # Confirm download success to client
+        print("File Sucessfully Downloaded. You can find it in your Downloads folder.")
+    except Exception as e:
+        print(f"Error, Please try again.\n{e}")
+        return
 
 # FUNCTION: UPLOAD
 def file_upload(client_socket, args, key):
@@ -100,9 +123,13 @@ def file_upload(client_socket, args, key):
 
     # Check if file exists before sending
     if os.path.exists(filepath):
-        client_socket.send(rsa.encrypt(f"{os.path.getsize(filepath)}".encode(FORMAT), key))  # Send file size
+        filesize = str(os.path.getsize(filepath))
+        client_socket.send(f"{filesize}".encode(FORMAT)) # Send file size
         with open(filepath, 'rb') as file:
-            client_socket.sendall(f"{file.read()}".encode(FORMAT))  # Send file data
+            # Encrypt and send File data
+            encryptor = AES.new(cipher_key, AES.MODE_EAX, nonce)
+            client_socket.sendall(encryptor.encrypt(file.read()))
+            del encryptor
         return 1
     else:
        print("File not found")
@@ -136,7 +163,7 @@ def main():
             waiting = file_upload(client, args, public_key)
         elif cmd.lower() == "download":
             file_download(client, args)
-            waiting = 0
+            waiting = 0 # In this case, the server's responses will be just the file data, so we will not wait for the server's to send something after the file is downloaded
         elif cmd.lower() == "logout":
             break
     print("Disconnectd from server.")
